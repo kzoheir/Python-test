affb421237d0b50c3bf9a1443bcd08684bc6255b
Khaled Zoheir <khaled.zoheir.kz@gmail.com>
2022-08-12 01:30:32 +0200

    Add unit test files

---
+++ basic_test.py
@@ -0,0 +1,12 @@
+import unittest
+import calculator
+
+class Testing(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(calculator.add(1, 2), 3)
+
+    def test_sum_float_failed(self):
+        self.assertEqual(calculator.add(1, 2), 3)
+
+if __name__ == '__main__':
+    unittest.main()
